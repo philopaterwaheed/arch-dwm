355e3f7322ef89f3c971e94b27bc2f28b60e17fe
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2022-08-15 21:58:18 +0000

    Add files via upload

--- dwm/config.h
+++ dwm/config.h
@@ -35,10 +35,10 @@ static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	 */ // rules for programs
+	/* class      instance    title       tags mask  iscentered    isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,          0, 		 1,    		 -1 },
+	{ "St",  NULL,       NULL,       1 << 8,    	  0,	         0,           -1 },
 };
 
 /* layout(s) */
