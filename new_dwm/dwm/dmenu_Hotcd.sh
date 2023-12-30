182914461037294de7efbbc05c79a5831248a5b7
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2023-12-30 08:43:43 +0200

    more cooler version

--- new_dwm/dwm/dmenu_Hotcd.sh
+++ new_dwm/dwm/dmenu_Hotcd.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 HOT_DIRS=("$HOME/Dev/" "$HOME/Hdd/downloads/" "$HOME/Downloads" "$HOME/Pics/") # the most visited dirictories in the system
-DIR=$(find "${HOT_DIRS[@]}" -type d ! -name '.*' |dmenu)
+HOT_DIRSLS=("$HOME/Hdd/" "$HOME/HDD1/" "$HOME/" ) # the most visited dirictories in the system using ls
+
+FOUND=$(find "${HOT_DIRS[@]}" -type d ! -name '.*')
+LSED=$(find "${HOT_DIRSLS[@]}" -maxdepth 1 -type d ! -name '.*')
+DIR=$(echo -e "$FOUND\n$LSED"|dmenu)
 if [[ -n "$DIR" ]]; then
 st -e bash --rcfile <(echo ". ~/.bashrc; cd $DIR")
 fi
