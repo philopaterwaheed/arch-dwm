7433e3d0acf4544e9b729063e37f4460268efba0
philosan <philopaterwaheed9@gmail.com>
2026-01-05 05:15:57 +0200

    feat: add additional productivity enhancements with new key mappings for editing, navigation, and coding efficiency

--- .config/new_nv/nvim/lua/philo/keymaps.lua
+++ .config/new_nv/nvim/lua/philo/keymaps.lua
@@ -345,3 +345,327 @@ map("n", "<leader>xx", "<cmd>source $MYVIMRC<cr>", { desc = "Reload config" })
 -------------------------------------------------------------------------------
 map("n", "<leader>ts", "<cmd>set spell!<cr>", { desc = "Toggle spell check" })
 
+-------------------------------------------------------------------------------
+-- ADDITIONAL PRODUCTIVITY ENHANCEMENTS
+-- New shortcuts for faster editing, navigation, and coding efficiency
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-- Smart Line Operations
+-------------------------------------------------------------------------------
+-- Delete line without yanking (useful when you don't want to pollute registers)
+map("n", "<leader>D", '"_dd', { desc = "Delete line (no yank)" })
+map("v", "<leader>D", '"_d', { desc = "Delete selection (no yank)" })
+
+-- Yank to end of line (consistent with D and C behavior)
+map("n", "Y", "y$", { desc = "Yank to end of line" })
+
+-- Yank to system clipboard
+map({ "n", "v" }, "<leader>Y", '"+y', { desc = "Yank to system clipboard" })
+map("n", "<leader>yy", '"+yy', { desc = "Yank line to system clipboard" })
+
+-- Paste from system clipboard
+map({ "n", "v" }, "<leader>P", '"+p', { desc = "Paste from system clipboard" })
+
+-------------------------------------------------------------------------------
+-- Quick Text Manipulation
+-------------------------------------------------------------------------------
+-- Change word under cursor (like ciw but faster)
+map("n", "<leader>cw", "ciw", { desc = "Change inner word" })
+
+-- Delete word under cursor without yank
+map("n", "<leader>dw", '"_diw', { desc = "Delete inner word (no yank)" })
+
+-- Sort lines in visual mode
+map("v", "<leader>ss", ":sort<cr>", { desc = "Sort selected lines" })
+map("v", "<leader>su", ":sort u<cr>", { desc = "Sort unique lines" })
+
+-- Wrap selection in quotes/brackets (works with nvim-surround but these are quick)
+map("v", '<leader>"', 'c"<C-r>""<Esc>', { desc = 'Wrap in double quotes' })
+map("v", "<leader>'", "c'<C-r>\"'<Esc>", { desc = "Wrap in single quotes" })
+map("v", "<leader>(", "c(<C-r>\")<Esc>", { desc = "Wrap in parentheses" })
+map("v", "<leader>[", "c[<C-r>\"]<Esc>", { desc = "Wrap in brackets" })
+map("v", "<leader>{", "c{<C-r>\"}<Esc>", { desc = "Wrap in braces" })
+
+-------------------------------------------------------------------------------
+-- Enhanced Navigation
+-------------------------------------------------------------------------------
+-- Beginning/end of line (easier than 0 and $)
+map({ "n", "v" }, "H", "^", { desc = "Go to first non-blank" })
+map({ "n", "v" }, "L", "$", { desc = "Go to end of line" })
+
+-- Quick paragraph navigation
+map({ "n", "v" }, "<C-j>", "}", { desc = "Next paragraph" })
+map({ "n", "v" }, "<C-k>", "{", { desc = "Previous paragraph" })
+
+-- Jump to matching bracket/brace
+map("n", "<Tab>", "%", { desc = "Jump to matching bracket" })
+
+-- Center screen after G and gg
+map("n", "G", "Gzz", { desc = "Go to end of file (centered)" })
+map("n", "gg", "ggzz", { desc = "Go to start of file (centered)" })
+
+-------------------------------------------------------------------------------
+-- Buffer & Tab Enhancements
+-------------------------------------------------------------------------------
+-- Quick buffer navigation without leader
+map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+
+-- Create new buffer
+map("n", "<leader>bn", "<cmd>enew<cr>", { desc = "New buffer" })
+
+-- Close buffer and move to previous
+map("n", "<leader>bD", "<cmd>bprevious<bar>bdelete #<cr>", { desc = "Delete buffer & go prev" })
+
+-- Tab management
+map("n", "<leader><Tab>n", "<cmd>tabnew<cr>", { desc = "New tab" })
+map("n", "<leader><Tab>c", "<cmd>tabclose<cr>", { desc = "Close tab" })
+map("n", "<leader><Tab>l", "<cmd>tabnext<cr>", { desc = "Next tab" })
+map("n", "<leader><Tab>h", "<cmd>tabprev<cr>", { desc = "Previous tab" })
+
+-------------------------------------------------------------------------------
+-- Window Management Enhancements
+-------------------------------------------------------------------------------
+-- Maximize/restore window (toggle)
+map("n", "<leader>wm", "<C-w>|<C-w>_", { desc = "Maximize window" })
+map("n", "<leader>w=", "<C-w>=", { desc = "Equalize windows" })
+
+-- Swap windows
+map("n", "<leader>wr", "<C-w>r", { desc = "Rotate windows" })
+map("n", "<leader>wR", "<C-w>R", { desc = "Rotate windows (reverse)" })
+map("n", "<leader>wx", "<C-w>x", { desc = "Swap with next window" })
+
+-- Close all other windows
+map("n", "<leader>wo", "<C-w>o", { desc = "Close other windows" })
+
+-------------------------------------------------------------------------------
+-- Search Enhancements
+-------------------------------------------------------------------------------
+-- Search current word without jumping
+map("n", "*", "*N", { desc = "Search word (stay)" })
+map("n", "#", "#N", { desc = "Search word backward (stay)" })
+
+-- Visual mode search for selected text
+map("v", "*", [[y/\V<C-R>=escape(@",'/\')<CR><CR>N]], { desc = "Search selection (stay)" })
+
+-------------------------------------------------------------------------------
+-- Improved Code Editing
+-------------------------------------------------------------------------------
+-- Reselect pasted text
+map("n", "gp", "`[v`]", { desc = "Reselect pasted text" })
+
+-- Select last changed/yanked text
+map("n", "gV", "`[v`]", { desc = "Select last changed text" })
+
+-- Split line at cursor (opposite of J)
+map("n", "S", "i<cr><Esc>^", { desc = "Split line at cursor" })
+
+-- Fix common typos while typing
+map("i", "teh", "the")
+map("i", "adn", "and")
+map("i", "waht", "what")
+
+-------------------------------------------------------------------------------
+-- LSP Quick Actions (Additional)
+-------------------------------------------------------------------------------
+-- Quick goto with centering
+map("n", "gd", function()
+  vim.lsp.buf.definition()
+  vim.schedule(function() vim.cmd("normal! zz") end)
+end, { desc = "Go to definition (centered)" })
+
+map("n", "gD", function()
+  vim.lsp.buf.declaration()
+  vim.schedule(function() vim.cmd("normal! zz") end)
+end, { desc = "Go to declaration (centered)" })
+
+map("n", "gi", function()
+  vim.lsp.buf.implementation()
+  vim.schedule(function() vim.cmd("normal! zz") end)
+end, { desc = "Go to implementation (centered)" })
+
+map("n", "gr", function()
+  vim.lsp.buf.references()
+end, { desc = "Find references" })
+
+-- Quick hover (K is more ergonomic)
+map("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+
+-- Diagnostic quick jump with preview
+map("n", "<leader>lp", function()
+  vim.diagnostic.goto_prev({ float = true })
+end, { desc = "Previous diagnostic" })
+
+map("n", "<leader>lN", function()
+  vim.diagnostic.goto_next({ float = true })
+end, { desc = "Next diagnostic" })
+
+-------------------------------------------------------------------------------
+-- Quick Comment Toggle (works with Comment.nvim)
+-------------------------------------------------------------------------------
+-- Ctrl+/ to toggle comment (common IDE binding)
+map("n", "<C-/>", function()
+  require("Comment.api").toggle.linewise.current()
+end, { desc = "Toggle comment" })
+
+map("v", "<C-/>", "<Esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", { desc = "Toggle comment" })
+
+-- Also map Ctrl+_ for terminals that send this for Ctrl+/
+map("n", "<C-_>", function()
+  require("Comment.api").toggle.linewise.current()
+end, { desc = "Toggle comment" })
+
+map("v", "<C-_>", "<Esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", { desc = "Toggle comment" })
+
+-------------------------------------------------------------------------------
+-- Telescope Power User Shortcuts
+-------------------------------------------------------------------------------
+-- Find in current directory
+map("n", "<leader>fd", function()
+  require("telescope.builtin").find_files({ cwd = vim.fn.expand("%:p:h") })
+end, { desc = "Find files in current dir" })
+
+-- Search marks
+map("n", "<leader>fm", "<cmd>Telescope marks<cr>", { desc = "Find marks" })
+
+-- Search registers
+map("n", "<leader>fR", "<cmd>Telescope registers<cr>", { desc = "Find registers" })
+
+-- Search jumplist
+map("n", "<leader>fj", "<cmd>Telescope jumplist<cr>", { desc = "Find jumplist" })
+
+-- Treesitter symbols (if installed)
+map("n", "<leader>fs", "<cmd>Telescope treesitter<cr>", { desc = "Treesitter symbols" })
+
+-- Search highlights
+map("n", "<leader>fH", "<cmd>Telescope highlights<cr>", { desc = "Find highlights" })
+
+-- Find TODOs (requires ripgrep)
+map("n", "<leader>ft", function()
+  require("telescope.builtin").grep_string({ search = "TODO|FIXME|HACK|NOTE|XXX", use_regex = true })
+end, { desc = "Find TODOs" })
+
+-------------------------------------------------------------------------------
+-- Quick Toggles (<leader>T prefix)
+-------------------------------------------------------------------------------
+-- Toggle relative line numbers
+map("n", "<leader>Tr", function()
+  vim.o.relativenumber = not vim.o.relativenumber
+end, { desc = "Toggle relative numbers" })
+
+-- Toggle line numbers entirely
+map("n", "<leader>Tn", function()
+  vim.o.number = not vim.o.number
+end, { desc = "Toggle line numbers" })
+
+-- Toggle wrap
+map("n", "<leader>Tw", "<cmd>set wrap!<cr>", { desc = "Toggle word wrap" })
+
+-- Toggle list (show invisible chars)
+map("n", "<leader>Tl", "<cmd>set list!<cr>", { desc = "Toggle list chars" })
+
+-- Toggle cursor line
+map("n", "<leader>Tc", "<cmd>set cursorline!<cr>", { desc = "Toggle cursor line" })
+
+-- Toggle virtual edit (allow cursor beyond line end)
+map("n", "<leader>Tv", function()
+  if vim.o.virtualedit == "" then
+    vim.o.virtualedit = "all"
+    vim.notify("Virtual edit: ON")
+  else
+    vim.o.virtualedit = ""
+    vim.notify("Virtual edit: OFF")
+  end
+end, { desc = "Toggle virtual edit" })
+
+-------------------------------------------------------------------------------
+-- Code Runner Shortcuts
+-------------------------------------------------------------------------------
+-- Run current file based on filetype
+map("n", "<leader>xf", function()
+  local ft = vim.bo.filetype
+  local file = vim.fn.expand("%:p")
+  local cmds = {
+    python = "python3 " .. file,
+    javascript = "node " .. file,
+    typescript = "ts-node " .. file,
+    lua = "lua " .. file,
+    sh = "bash " .. file,
+    rust = "cargo run",
+    c = "gcc -o /tmp/a.out " .. file .. " && /tmp/a.out",
+    cpp = "g++ -o /tmp/a.out " .. file .. " && /tmp/a.out",
+  }
+  local cmd = cmds[ft]
+  if cmd then
+    vim.cmd("TermExec cmd='" .. cmd .. "'")
+  else
+    vim.notify("No runner for filetype: " .. ft, vim.log.levels.WARN)
+  end
+end, { desc = "Run current file" })
+
+-------------------------------------------------------------------------------
+-- Improved Insert Mode Shortcuts
+-------------------------------------------------------------------------------
+-- Quick save from insert mode
+map("i", "<C-s>", "<Esc><cmd>w<cr>", { desc = "Save file" })
+
+-- Undo from insert mode
+map("i", "<C-z>", "<C-o>u", { desc = "Undo" })
+
+-- Quick way to add semicolon/comma at end of line
+map("i", "<C-;>", "<End>;", { desc = "Add semicolon at EOL" })
+map("i", "<C-,>", "<End>,", { desc = "Add comma at EOL" })
+
+-- Jump to end of line in insert mode (more ergonomic than <End>)
+map("i", "<C-e>", "<End>", { desc = "Go to end of line" })
+map("i", "<C-a>", "<Home>", { desc = "Go to start of line" })
+
+-------------------------------------------------------------------------------
+-- Session Management (Lightweight)
+-------------------------------------------------------------------------------
+-- Quick session save/load using built-in mksession
+map("n", "<leader>Ss", function()
+  local session_dir = vim.fn.stdpath("data") .. "/sessions/"
+  vim.fn.mkdir(session_dir, "p")
+  local session_file = session_dir .. vim.fn.input("Session name: ") .. ".vim"
+  vim.cmd("mksession! " .. session_file)
+  vim.notify("Session saved: " .. session_file)
+end, { desc = "Save session" })
+
+map("n", "<leader>Sl", function()
+  local session_dir = vim.fn.stdpath("data") .. "/sessions/"
+  local sessions = vim.fn.glob(session_dir .. "*.vim", false, true)
+  if #sessions == 0 then
+    vim.notify("No sessions found", vim.log.levels.WARN)
+    return
+  end
+  vim.ui.select(sessions, { prompt = "Load session:" }, function(choice)
+    if choice then
+      vim.cmd("source " .. choice)
+    end
+  end)
+end, { desc = "Load session" })
+
+-------------------------------------------------------------------------------
+-- Quick Fix Editing Mistakes
+-------------------------------------------------------------------------------
+-- Swap two characters
+map("n", "<leader>xc", "xp", { desc = "Swap chars forward" })
+
+-- Change case of word
+map("n", "<leader>xu", "gUiw", { desc = "UPPERCASE word" })
+map("n", "<leader>xl", "guiw", { desc = "lowercase word" })
+map("n", "<leader>xt", "g~iw", { desc = "Toggle case word" })
+
+-------------------------------------------------------------------------------
+-- Navigation History
+-------------------------------------------------------------------------------
+-- Jump back/forward (like browser history)
+map("n", "<A-Left>", "<C-o>", { desc = "Jump back" })
+map("n", "<A-Right>", "<C-i>", { desc = "Jump forward" })
+
+-- Easier access to jumplist navigation
+map("n", "<leader>jo", "<C-o>", { desc = "Jump older" })
+map("n", "<leader>ji", "<C-i>", { desc = "Jump newer" })
+
