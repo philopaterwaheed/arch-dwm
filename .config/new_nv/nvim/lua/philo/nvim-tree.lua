cfc8504fb848df64a4ae3a96908fd57ba0dde425
philosan <philopaterwaheed9@gmail.com>
2026-01-05 11:24:30 +0200

    refactor: update diagnostics configuration in nvim-tree setup

--- .config/new_nv/nvim/lua/philo/nvim-tree.lua
+++ .config/new_nv/nvim/lua/philo/nvim-tree.lua
@@ -105,17 +105,22 @@ M.setup = function()
       ignore_list = {},
     },
 
-    diagnostics = {
-      enable = true,
-      show_on_dirs = true,
+  diagnostics = {
+      enable = false,
+      show_on_dirs = false,
+      show_on_open_dirs = true,
+      debounce_delay = 50,
+      severity = {
+        min = vim.diagnostic.severity.HINT,
+        max = vim.diagnostic.severity.ERROR
+      },
       icons = {
-        hint = "󰌵",
-        info = "",
-        warning = "",
-        error = "",
+        hint = "💡",
+        info = "🛈",
+        warning = "⚠️",
+        error = "❌",
       },
     },
-
     filters = {
       dotfiles = false,
       custom = { ".DS_Store", "__pycache__", ".pytest_cache" },
